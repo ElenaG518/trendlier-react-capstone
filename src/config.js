6d9f82307429fb92e32c8d05d11cafeca311fbd3
Elena Granados <patty518@yahoo.com>
2019-04-09 01:19:13 -0700

    modify config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-//  ||  "https://trendlier-server-capstone.herokuapp.com";
-// process.env.REACT_APP_API_BASE_URL ||  "http://localhost:8080";
+export const API_BASE_URL = "https://trendlier-server-capstone.herokuapp.com" ||  "http://localhost:8080";
