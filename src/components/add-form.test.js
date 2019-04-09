83aca78d20f6a9df6b3c8d10d52001ede34ec3e7
Elena Granados <patty518@yahoo.com>
2019-04-08 23:55:37 -0700

    fix test for edit form

--- src/components/add-form.test.js
+++ src/components/add-form.test.js
@@ -8,14 +8,14 @@ describe('<AddForm />', () => {
         shallow(<AddForm />);
     });
 
-    it('Renders the add button initially', () => {
+    it('Renders the add item button initially', () => {
         const wrapper = shallow(<AddForm />);
-        expect(wrapper.hasClass('add-button')).toEqual(true);
+        expect(wrapper.hasClass('add-item')).toEqual(true);
     });
 
     it('Should render the add form when adding', () => {
         const wrapper = shallow(<AddForm />);
-        wrapper.instance().setAdding(true);
+        wrapper.instance().setEditing(true);
         wrapper.update();
         expect(wrapper.hasClass('add-form')).toEqual(true);
     });
@@ -24,14 +24,14 @@ describe('<AddForm />', () => {
         const wrapper = shallow(<AddForm />);
         // wrapper.instance().setAdding(false);
         wrapper.simulate('click');
-        expect(wrapper.state('adding')).toEqual(true);
+        expect(wrapper.state('editing')).toEqual(true);
     });
 
     it('Should fire the onAdd callback when the form is submitted', () => {
         const callback = jest.fn();
         const wrapper = mount(<AddForm onAdd={callback} />);
         const value = 'Foobar';
-        wrapper.instance().setAdding(true);
+        wrapper.instance().setEditing(true);
         wrapper.update();
         wrapper.find('textarea[type="text"]').instance().value = value;
         wrapper.simulate('submit');
@@ -41,7 +41,7 @@ describe('<AddForm />', () => {
     it('Should not fire onAdd if the input is empty', () => {
         const callback = jest.fn();
         const wrapper = mount(<AddForm onAdd={callback} />);
-        wrapper.instance().setAdding(true);
+        wrapper.instance().setEditing(true);
         wrapper.simulate('submit');
         expect(callback).not.toHaveBeenCalled();
     });
